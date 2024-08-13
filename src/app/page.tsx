45c1484f61441e2355777268440232c69415db1e
AlySerry <alyserry@outlook.com>
2024-08-13 13:08:00 +0300

    front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 // src/app/page.tsx
-import React from 'react';
+import React, { Suspense } from 'react';
 import MovieGrid from '@/components/MovieGrid';
 
-export default function Home() {
-	return (
-		<MovieGrid/>
-	);
-}
+const HomePage: React.FC = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <MovieGrid />
+    </Suspense>
+  );
+};
+
+export default HomePage;
